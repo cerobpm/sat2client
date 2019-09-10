8d68234e1537a7f9553aaf5e7fa665000dc448a1
cerobpm <40410912+cerobpm@users.noreply.github.com>
2019-09-10 12:15:01 -0300

    Update update_horario.sh

--- update_horario.sh
+++ update_horario.sh
@@ -3,8 +3,8 @@
 sd=$(date -d "now - 1 days" +%Y-%m-%d)
 ed=$(date -d "tomorrow" +%Y-%m-%d)
 
-nodejs index.js Sat2:GetEquipos RHN 1R2H
-nodejs index.js Sat2:GetHistoricosPorFechas RHN 1R2H $sd $ed
+nodejs index.js Sat2:GetEquipos $user $pass
+nodejs index.js Sat2:GetHistoricosPorFechas $user $pass $sd $ed
 nodejs index.js Sat2:updateCount all $sd $ed
 nodejs index.js Sat2:updateReportes anio $sd $ed
 nodejs index.js Sat2:updateReportes mes $sd $ed
